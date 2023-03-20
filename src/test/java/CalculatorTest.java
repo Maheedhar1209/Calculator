0aaa965f8f13fc4d78c5fb3ff8f7ef37a12ab24d
Maheedhar1209 <122871706+Maheedhar1209@users.noreply.github.com>
2023-03-20 18:25:00 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,26 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class CalculatorTest {
 
-    private final Calculator mycalc = new Calculator();
     @Test
     public void square_root_test() {
-        var res = mycalc.square_root(4.0);
+        var res = Calculator.square_root(4.0);
         assertEquals(2.0,res);
     }
     @Test
     public void power_test(){
 
-        assertEquals(8.0,mycalc.power(2.0,3.0));
+        assertEquals(8.0,Calculator.power(2.0,3.0));
     }
     @Test
     public void factorial(){
 
-        assertEquals(2.0,mycalc.factorial(2.0));
+        assertEquals(2.0,Calculator.factorial(2.0));
     }
     @Test
     public void log(){
 
-        assertEquals(2.30,mycalc.log(10.0),0.02f);
+        assertEquals(2.30,Calculator.log(10.0),0.02f);
 
     }
 }
